d460b500697289c2f8dd3054a999e7618f4f0809
faisal8373 <faisal205.vu@gmail.com>
2024-01-17 11:47:04 +0500

    update

---
+++ pharmacy.sql
@@ -0,0 +1,12 @@
+-- SELECT * FROM Tablet_Pharmacy.dbo.tblMyStock;
+
+
+-- SELECT * FROM Tablet_Pharmacy.dbo.tblPDetail;
+
+
+-- SELECT Item, PurchasePrice, PackUnits, Prdcode, BatchExpiry, CostPrice 
+-- FROM Tablet_Pharmacy.dbo.tblMyStock
+-- INNER JOIN Tablet_Pharmacy.dbo.tblPDetail ON tblPDetail.AutoCode = Tablet_Pharmacy.dbo.tblMyStock.Prdcode;
+
+-- SELECT * from Tablet_Pharmacy.dbo.tblStdtl2;
+
